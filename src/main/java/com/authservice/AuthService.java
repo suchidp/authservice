5c504f7f1b16e0c76951607112842a65db2445ab
Suchid <suchidp@cybage.com>
2023-05-23 12:20:37 +0530

    projectsetup

---
+++ src/main/java/com/authservice/AuthService.java
@@ -0,0 +1,13 @@
+package com.authservice;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+;
+
+
+@SpringBootApplication
+public class AuthService {
+	public static void main(String[] args) {
+		SpringApplication.run(AuthService.class, args);
+	}
+
+}
